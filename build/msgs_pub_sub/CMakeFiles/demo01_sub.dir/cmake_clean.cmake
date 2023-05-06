56f38a1eb44b4a4fe68650f16fa6d8bb8a210dd6
sulihaotian <1156996101@qq.com>
2023-05-06 00:39:14 -0700

    ros_pub_sub

---
+++ build/msgs_pub_sub/CMakeFiles/demo01_sub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/ros/demo02_ws/devel/lib/msgs_pub_sub/demo01_sub"
+  "/home/ros/demo02_ws/devel/lib/msgs_pub_sub/demo01_sub.pdb"
+  "CMakeFiles/demo01_sub.dir/src/demo01_sub.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo01_sub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/msgs_pub_sub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros/demo02_ws/src/msgs_pub_sub
+# Build directory: /home/ros/demo02_ws/build/msgs_pub_sub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
